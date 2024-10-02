4d3f58cd7e7dda9b9712b3333a3d6b57ac7f3081
DZ10LUIS <romeroluis101003@gmail.com>
2024-10-02 13:09:34 -0600

    Implementacion De conexion a firebase

--- app/src/main/java/com/develompent/controlschool/MainActivity.java
+++ app/src/main/java/com/develompent/controlschool/MainActivity.java
@@ -1,6 +1,9 @@
 package com.develompent.controlschool;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,24 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button Access;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        Access = findViewById(R.id.acceder);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
         });
+
+
+    }
+    public void Acceder (View view){
+        Intent intent = new Intent(this, Menu.class);
+        startActivity(intent);
     }
 }
