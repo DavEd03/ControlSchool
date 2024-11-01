91fdde4b7d48b556a82fd89396efa02fc8732eb5
DZ10LUIS <123421567+DZ10LUIS@users.noreply.github.com>
2024-11-01 12:02:20 -0600

    implementacion de conexion

--- app/src/main/java/com/develompent/controlschool/Datos.java
+++ app/src/main/java/com/develompent/controlschool/Datos.java
@@ -15,11 +15,18 @@ public Datos (String nombre, String apellidos, String carrera, String correo, St
     this.cuatrimestre = cuatrimestre;
     this.matricula = matricula;
 }
-public String getNombre() {return nombre;}
+    public String getNombre() {return nombre;}
     public String getApellidos(){return apellidos;}
     public String getCarrera(){return carrera;}
     public String getCorreo() {return correo;}
     public String getCuatrimestre() {return cuatrimestre;}
     public String getMatricula() {return matricula;}
+
+    public void setNombre(String nombre) {this.nombre = nombre;}
+    public void setApellidos(String apellidos) {this.apellidos = apellidos;}
+    public void setCarrera(String carrera) {this.carrera = carrera;}
+    public void setCorreo(String correo) {this.correo = correo;}
+    public void setCuatrimestre(String cuatrimestre) {this.cuatrimestre = cuatrimestre;}
+    public void setMatricula(String matricula) {this.matricula = matricula;}
 }
 
